1c1d2e2722914270b060dd459217d83e6d093011
Daniel Mardunovich <mardunovich@gmail.com>
2023-02-04 11:07:47 +0100

    Line stuff

---
+++ GlobalGJ/Assets/Manager/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+    
+    [SerializeField]
+    GameState state;
+    GameState previousState;
+
+    public static event Action<GameState> OnStateChange;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        UpdateState(GameState.MAINMENU);
+    }
+
+    private void Update()
+    {
+        if (state == GameState.PAUSED)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                UpdateState(GameState.UNPAUSE);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                UpdateState(GameState.PAUSED);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                UpdateState(GameState.MAINMENU);
+            }
+        }
+    }
+
+    void UpdateState(GameState _state)
+    {
+        previousState = this.state;
+        this.state = _state;
+
+        switch (_state)
+        {
+            case GameState.MAINMENU:
+                break;
+            case GameState.PAUSED:
+                Time.timeScale = 0f;
+                break;
+            case GameState.UNPAUSE:
+                Time.timeScale = 1f;
+                break;
+            case GameState.SETUP:
+                break;
+            case GameState.PLAY:
+                break;
+            case GameState.GAMEOVER:
+                break;
+            default:
+                break;
+
+        }
+        OnStateChange?.Invoke(_state);
+    }
+
+    GameState GetCurrentState()
+    {
+        return state;
+    }
+    
+}
+public enum GameState { MAINMENU, PAUSED, UNPAUSE, SETUP, PLAY, GAMEOVER }
---
+++ GlobalGJ/Assets/Manager/Scripts/TEST.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+
+    void LOGTHIS()
+    {
+        Debug.Log("MAIN MENU");
+    }
+
+    bool isDrawing = false;
+    Vector3 startPos, drawPos;
+
+    GameObject lineObject;
+
+    void GameManagerOnStateChange(GameState _state)
+    {
+        switch (_state)
+        {
+            case GameState.MAINMENU:
+                Debug.Log("MAIN MENU");
+                break;
+            case GameState.PAUSED:
+                Debug.Log("PAUSED");
+                break;
+            case GameState.UNPAUSE:
+                Debug.Log("UNPAUSE");
+                break;
+            case GameState.SETUP:
+                Debug.Log("SETUP");
+                break;
+            case GameState.PLAY:
+                Debug.Log("PLAY");
+                break;
+            case GameState.GAMEOVER:
+                Debug.Log("GAME OVER");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void Awake()
+    {
+        GameManager.OnStateChange += GameManagerOnStateChange;
+        
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnStateChange -= GameManagerOnStateChange;
+    }
+
+    private void Start()
+    {
+        lineObject = GameObject.Find("LineDrawer");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lineObject.GetComponent<LineRenderer>().SetPosition(0, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            isDrawing = true;
+        }
+
+        if (isDrawing)
+        {
+            drawPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            {
+                Debug.Log(drawPos.x);
+                Debug.Log(drawPos.y);
+            }
+
+            lineObject.GetComponent<LineRenderer>().SetPosition(1, new Vector3(startPos.x + drawPos.x, startPos.y + drawPos.y, 0));
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDrawing = false;
+        }
+    }
+}
